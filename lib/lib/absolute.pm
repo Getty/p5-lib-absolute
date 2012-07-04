f68729f8b9815a34ac63e2cc5792c148a52aecac
Torsten Raudssus <torsten@raudss.us>
2012-07-04 17:13:54 -0400

    Added documentation

--- lib/lib/absolute.pm
+++ lib/lib/absolute.pm
@@ -23,3 +23,33 @@ sub import {
 }
 
 1;
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+  use lib::absolute;
+
+  use lib::absolute -hard; # crashs on non existing directories
+
+=head1 DESCRIPTION
+
+This package converts on load all your @INC path into absolute paths, if you have "." in your path, it gets additionally
+added again (and also get added as absolute path).
+
+=head1 SUPPORT
+
+IRC
+
+  Join #perl-help on irc.perl.org. Highlight Getty for fast reaction :).
+
+Repository
+
+  http://github.com/Getty/p5-lib-absolute
+  Pull request and additional contributors are welcome
+ 
+Issue Tracker
+
+  http://github.com/Getty/p5-lib-absolute/issues
+
+
